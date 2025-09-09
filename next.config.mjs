5d246d838871432e4cbe987ae6417dd493634d3c
Akshat-Bindal <biakshat31@gmail.com>
2025-09-09 10:42:19 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'export',
-  images: { unoptimized: true }   // 👈 this is the important line
+  images: { unoptimized: true }, // required since Next Image optimizer doesn’t work on static hosting
+  trailingSlash: true,           // optional (keeps routes as /about/ -> about/index.html)
 };
 
 export default nextConfig;
